b1ef9754e45859e51cbde1278e7bb58811edbbb7
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-08-25 21:32:57 +0530

    calculate sum functionality

--- isTriangle/app.js
+++ isTriangle/app.js
@@ -1,3 +1,8 @@
 const angleInputs = document.querySelectorAll(".angle");
 const calculate = document.querySelector("#calculate");
 const output = document.querySelector("#output");
+
+function calculateSum(angle1, angle2, angle3) {
+    let sum = angle1 + angle2 + angle3
+    return sum
+}
