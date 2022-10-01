452d9c19b44ac640fba1cc460fc6f07a32a897ff
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-10-01 20:39:10 +0530

    refactored output

--- triangle-quiz/app.js
+++ triangle-quiz/app.js
@@ -1,21 +1,23 @@
-const quizForm= document.querySelector(".quiz-form");
+const quizForm = document.querySelector(".quiz-form");
 const submitAnswer = document.querySelector("#submit-answer");
 const output = document.querySelector("#output");
 
-submitAnswer.addEventListener("click",calculateScore);
+submitAnswer.addEventListener("click", calculateScore);
 
-function calculateScore(){
-let index=0;
-let score=0;
-let answers=["90","75","60","61","100"];
+function calculateScore() {
+  let index = 0;
+  let score = 0;
+  let answers = ["90", "75", "60", "61", "100"];
 
-
-    let formResults=new FormData(quizForm);
-    for(value of formResults.values()){
-        if(value==answers[index]){
-score++;
-        }
-        index++;
+  let formResults = new FormData(quizForm);
+  for (value of formResults.values()) {
+    if (value == answers[index]) {
+      score++;
     }
-output.innerText=`Your Score Is : ${score}`;
+    index++;
+  }
+  showOutput(score);
+}
+function showOutput(rslt) {
+  output.innerText = `Your Score Is : ${rslt}`;
 }
